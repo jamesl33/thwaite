a5da82602fd98d7303b35f3477e7fc29e6cdcf02
James Lee <jamesl33info@gmail.com>
2025-01-25 21:34:40 +0000

    Document G0 pruning table generation

--- src/solver/group_one/table.rs
+++ src/solver/group_one/table.rs
@@ -24,24 +24,33 @@ fn g0() -> Table {
     /// DEPTH - TODO
     const DEPTH: usize = 7;
 
+    // We initialize the pruning table at the max depth, and search for the cheaper distances
     let mut tab: Table = Table { data: [DEPTH; SIZE] };
 
+    // The first parent, is the solved cube
     let mut parents: VecDeque<Cube> = VecDeque::from(vec![Cube::new(); 1]);
 
-    // TODO
+    // Which has a distance of zero
     tab.data[0] = 0;
 
+    // Apply 6 breadth first searches by applying turns to the cube, and recording the depth from the solved state, in
+    // the pruning table.
     for depth in 1..DEPTH {
-        parents = dfs(&mut tab, depth, &mut parents);
+        parents = bfs(&mut tab, depth, &mut parents);
     }
 
     tab
 }
 
-/// dfs - TODO
-fn dfs(tab: &mut Table, depth: usize, parents: &mut VecDeque<Cube>) -> VecDeque<Cube> {
-    let mut children: VecDeque<Cube> = VecDeque::with_capacity((18 as usize).pow(depth as u32));
+/// bfs - TODO
+fn bfs(tab: &mut Table, depth: usize, parents: &mut VecDeque<Cube>) -> VecDeque<Cube> {
+    // Retrieve the number of valid moves in this group
+    let valid = Group::Zero.moves().len();
 
+    // Preallocate enough space for all the possible turns, in G0 there's valid turns per-level
+    let mut children: VecDeque<Cube> = VecDeque::with_capacity(valid.pow(depth as u32));
+
+    // Go through all the parent cubes, and visit them
     while let Some(cube) = parents.pop_front() {
         visit(depth, cube, tab, &mut children)
     }
@@ -58,6 +67,7 @@ fn visit(depth: usize, cube: Cube, tab: &mut Table, children: &mut VecDeque<Cube
 
         let i = idx(cube.edge_orientations());
 
+        // Only update the pruning table, if we've found a shorter path
         if i != 0 && tab.data[i] > depth {
             tab.data[i] = depth;
         }
